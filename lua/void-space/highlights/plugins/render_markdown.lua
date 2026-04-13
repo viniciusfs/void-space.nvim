32ef2b520f3434d117124d7eec191bd9eae45e31
Vinicius Figueiredo <vinicius.figueiredo@bemobi.com>
2026-04-13 18:15:39 -0300

    fix(highlights/render_markdown): transparent path for headings/code, bullet uses fg_dim not red

--- lua/void-space/highlights/plugins/render_markdown.lua
+++ lua/void-space/highlights/plugins/render_markdown.lua
@@ -3,21 +3,23 @@ local M = {}
 function M.get(c, opts)
 	local hl = {}
 
-	hl.RenderMarkdownH1 = { fg = c.yellow, bg = c.bg_float, bold = true }
-	hl.RenderMarkdownH2 = { fg = c.blue, bg = c.bg_float, bold = true }
-	hl.RenderMarkdownH3 = { fg = c.cyan, bg = c.bg_float, bold = true }
-	hl.RenderMarkdownH4 = { fg = c.green, bg = c.bg_float, bold = true }
-	hl.RenderMarkdownH5 = { fg = c.orange, bg = c.bg_float, bold = true }
-	hl.RenderMarkdownH6 = { fg = c.purple, bg = c.bg_float, bold = true }
-	hl.RenderMarkdownH1Bg = { bg = c.bg_float }
-	hl.RenderMarkdownH2Bg = { bg = c.bg_float }
-	hl.RenderMarkdownH3Bg = { bg = c.bg_float }
-	hl.RenderMarkdownH4Bg = { bg = c.bg_float }
-	hl.RenderMarkdownH5Bg = { bg = c.bg_float }
-	hl.RenderMarkdownH6Bg = { bg = c.bg_float }
-	hl.RenderMarkdownCode = { bg = c.bg_float }
-	hl.RenderMarkdownCodeInline = { fg = c.green, bg = c.bg_float }
-	hl.RenderMarkdownBullet = { fg = c.red }
+	local bg_float = opts.transparent and c.none or c.bg_float
+
+	hl.RenderMarkdownH1 = { fg = c.yellow, bg = bg_float, bold = true }
+	hl.RenderMarkdownH2 = { fg = c.blue, bg = bg_float, bold = true }
+	hl.RenderMarkdownH3 = { fg = c.cyan, bg = bg_float, bold = true }
+	hl.RenderMarkdownH4 = { fg = c.green, bg = bg_float, bold = true }
+	hl.RenderMarkdownH5 = { fg = c.orange, bg = bg_float, bold = true }
+	hl.RenderMarkdownH6 = { fg = c.purple, bg = bg_float, bold = true }
+	hl.RenderMarkdownH1Bg = { bg = bg_float }
+	hl.RenderMarkdownH2Bg = { bg = bg_float }
+	hl.RenderMarkdownH3Bg = { bg = bg_float }
+	hl.RenderMarkdownH4Bg = { bg = bg_float }
+	hl.RenderMarkdownH5Bg = { bg = bg_float }
+	hl.RenderMarkdownH6Bg = { bg = bg_float }
+	hl.RenderMarkdownCode = { bg = bg_float }
+	hl.RenderMarkdownCodeInline = { fg = c.green, bg = bg_float }
+	hl.RenderMarkdownBullet = { fg = c.fg_dim }
 	hl.RenderMarkdownQuote = { fg = c.fg_dim, italic = opts.italic_comments }
 	hl.RenderMarkdownDash = { fg = c.sel }
 	hl.RenderMarkdownLink = { fg = c.blue, underline = true }
